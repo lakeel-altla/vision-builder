dba8c6c834b404033acec6413f1017e01aca27c3
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2017-07-10 16:02:23 +0900

    [UPDATE] Remove unused subtree models

--- subtree/android-binding/library/src/main/java/com/lakeel/altla/android/binding/BindingMode.java
+++
@@ -1,9 +0,0 @@
-package com.lakeel.altla.android.binding;
-
-public enum BindingMode {
-    DEFAULT,
-    ONE_WAY,
-    TWO_WAY,
-    ONE_TIME,
-    ONE_WAY_TO_SOURCE
-}
--- subtree/android-binding/library/src/main/java/com/lakeel/altla/android/binding/Unbindable.java
+++
@@ -1,6 +0,0 @@
-package com.lakeel.altla.android.binding;
-
-public interface Unbindable {
-
-    void unbind();
-}
